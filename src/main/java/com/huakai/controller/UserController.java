45fd07e273baa4da3c0518c66ffabbfcfb1b7c4d
huakaimay <huakaimay@gmail.com>
2021-08-30 14:08:21 +0800

    src

--- src/main/java/com/huakai/controller/UserController.java
+++ src/main/java/com/huakai/controller/UserController.java
@@ -1,12 +1,6 @@
 package com.huakai.controller;
 
-import com.huakai.dto.User;
-import com.huakai.mapper.UserMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * @author: huakaimay
@@ -15,13 +9,5 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    @Autowired
-    private UserMapper userMapper;
-
-    @GetMapping("/user")
-    public String getUser() {
-        List<User> user = userMapper.getUser();
-        return "name: " + user.get(0).getName() + ", age: " + user.get(0).getAge();
-    }
 
 }
