e381dc09c2a1a107589755c51bf2d80f5672c07e
huakaimay <huakaimay@gmail.com>
2021-09-03 15:27:55 +0800

    数据库脚本

--- seckill.sql
+++ seckill.sql
@@ -11,11 +11,113 @@
  Target Server Version : 80011
  File Encoding         : 65001
 
- Date: 30/08/2021 14:02:56
+ Date: 03/09/2021 15:26:47
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for item
+-- ----------------------------
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) NOT NULL DEFAULT '' COMMENT '商品名称',
+  `price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `sales` int(11) NOT NULL DEFAULT '0',
+  `img_url` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `description` varchar(500) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of item
+-- ----------------------------
+BEGIN;
+INSERT INTO `item` VALUES (5, '小米手机', 1900.00, 15, '', '小米手机，手机中的战斗机');
+INSERT INTO `item` VALUES (6, '小米手机2', 2333.00, 203, '', '小米牛逼战斗机');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for item_stock
+-- ----------------------------
+DROP TABLE IF EXISTS `item_stock`;
+CREATE TABLE `item_stock` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_id` int(11) NOT NULL DEFAULT '0',
+  `stock` int(11) NOT NULL DEFAULT '0' COMMENT '库存',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of item_stock
+-- ----------------------------
+BEGIN;
+INSERT INTO `item_stock` VALUES (3, 5, 286);
+INSERT INTO `item_stock` VALUES (4, 6, 195);
+COMMIT;
+
+-- ----------------------------
+-- Table structure for order_info
+-- ----------------------------
+DROP TABLE IF EXISTS `order_info`;
+CREATE TABLE `order_info` (
+  `id` varchar(32) NOT NULL DEFAULT '',
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `item_id` int(11) NOT NULL DEFAULT '0',
+  `price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `amount` int(11) NOT NULL DEFAULT '0',
+  `total_price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `promo_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of order_info
+-- ----------------------------
+BEGIN;
+INSERT INTO `order_info` VALUES ('2021090300000800', 15, 5, 99.00, 1, 99.00, 1);
+COMMIT;
+
+-- ----------------------------
+-- Table structure for promo
+-- ----------------------------
+DROP TABLE IF EXISTS `promo`;
+CREATE TABLE `promo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_id` int(11) NOT NULL DEFAULT '0',
+  `promo_name` varchar(255) NOT NULL DEFAULT '',
+  `price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `start_date` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `end_date` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of promo
+-- ----------------------------
+BEGIN;
+INSERT INTO `promo` VALUES (1, 5, '小米手机抢购', 99.00, '2021-09-03 15:02:00', '2021-09-03 15:59:00');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for sequence_info
+-- ----------------------------
+DROP TABLE IF EXISTS `sequence_info`;
+CREATE TABLE `sequence_info` (
+  `name` varchar(255) NOT NULL,
+  `current_value` int(11) NOT NULL DEFAULT '0',
+  `step` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of sequence_info
+-- ----------------------------
+BEGIN;
+INSERT INTO `sequence_info` VALUES ('order_info', 9, 1);
+COMMIT;
 
 -- ----------------------------
 -- Table structure for user_info
@@ -26,11 +128,19 @@ CREATE TABLE `user_info` (
   `name` varchar(64) NOT NULL DEFAULT '',
   `gender` tinyint(4) NOT NULL DEFAULT '0' COMMENT '1男性 2女性',
   `age` int(11) NOT NULL DEFAULT '0',
-  `telephone` varchar(0) NOT NULL DEFAULT '',
-  `register_mode` varchar(0) NOT NULL DEFAULT '' COMMENT '// byphone,bywechat,byalipay\n',
+  `telephone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `register_mode` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '// byphone,bywechat,byalipay\n',
   `third_part_id` varchar(64) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of user_info
+-- ----------------------------
+BEGIN;
+INSERT INTO `user_info` VALUES (11, '小卡', 1, 22, '123456', 'byalipay', '');
+INSERT INTO `user_info` VALUES (14, '小红', 2, 32, '123123', 'byalipay', '');
+COMMIT;
 
 -- ----------------------------
 -- Table structure for user_password
@@ -41,6 +151,16 @@ CREATE TABLE `user_password` (
   `encrypt_password` varchar(128) NOT NULL DEFAULT '',
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of user_password
+-- ----------------------------
+BEGIN;
+INSERT INTO `user_password` VALUES (1, 'sudhfsjdopp', 1);
+INSERT INTO `user_password` VALUES (2, 'qKfCOSY3bqsFyJ6mAai5nw==', 11);
+INSERT INTO `user_password` VALUES (5, 'qPXxZ/RPSWTmyZje6CcRDA==', 14);
+INSERT INTO `user_password` VALUES (6, 'qPXxZ/RPSWTmyZje6CcRDA==', 15);
+COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
