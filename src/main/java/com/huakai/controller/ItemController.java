0b05da037ffef602e91e0f619e23a5105b36983a
huakai <huakaimay@gmail.com>
2023-05-18 15:49:03 +0800

    添加注释

--- src/main/java/com/huakai/controller/ItemController.java
+++ src/main/java/com/huakai/controller/ItemController.java
@@ -59,11 +59,18 @@ public class ItemController {
         return CommonReturnType.create(itemDtos);
     }
 
+    /**
+     * Get方法的映射控制器，用于获取指定id的数据。
+     *
+     * @param id 待查询的数据id
+     * @return 返回CommonReturnType类型的结果，包含查询得到的数据
+     * @throws BussinesssError 在查询过程中出现业务异常
+     */
     @GetMapping("/get")
     public CommonReturnType get(@RequestParam("id") Integer id) throws BussinesssError {
-
+        // 从redis数据库查询id对应的数据信息
         ItemDto itemDto = redisService.get("item_" + id, ItemDto.class);
-
+        // 若查询到的数据为空，则从数据库中查询，然后存入redis
         if(ObjectUtils.isEmpty(itemDto)) {
             itemDto =itemService.getItemDetailById(id);
             if(!ObjectUtils.isEmpty(itemDto)) {
@@ -72,17 +79,16 @@ public class ItemController {
                 throw new BussinesssError(ErrorEnum.PARAMTER_VALIDATION_ERROR, "数据异常");
             }
         }
-
-
+        // 获取查询到的商品的优惠信息
         PromoDto promoDto = itemDto.getPromoDto();
+        // 若秒杀信息为空，则默认设置为没有秒杀
         if (ObjectUtils.isEmpty(promoDto)) {
             PromoDto innerPromo = new PromoDto();
             innerPromo.setStatus(0);
             itemDto.setPromoDto(innerPromo);
         }
-
+        // 返回查询到的商品信息
         return CommonReturnType.create(itemDto);
-
     }
 
 }
