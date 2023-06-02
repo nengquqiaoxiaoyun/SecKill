79fbc20a119b7e0da17c0a732608d69a2e28d238
huakai <huakaimay@gmail.com>
2023-06-02 09:57:08 +0800

    缓存

--- src/main/java/com/huakai/service/impl/OrderServiceImpl.java
+++ src/main/java/com/huakai/service/impl/OrderServiceImpl.java
@@ -1,20 +1,19 @@
 package com.huakai.service.impl;
 
+import com.google.gson.Gson;
+import com.huakai.config.RedisService;
 import com.huakai.controller.dto.ItemDto;
-import com.huakai.controller.dto.UserDto;
 import com.huakai.error.BussinesssError;
 import com.huakai.error.ErrorEnum;
-import com.huakai.mapper.ItemDOMapper;
 import com.huakai.mapper.OrderDoMapper;
 import com.huakai.mapper.SequenceDOMapper;
 import com.huakai.mapper.UserDOMapper;
-import com.huakai.mapper.dataobject.ItemDO;
 import com.huakai.mapper.dataobject.OrderDo;
 import com.huakai.mapper.dataobject.SequenceDO;
 import com.huakai.mapper.dataobject.UserDO;
 import com.huakai.service.ItemService;
 import com.huakai.service.OrderService;
-import com.huakai.service.UserService;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -23,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author: huakaimay
@@ -43,6 +43,9 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private SequenceDOMapper sequenceDOMapper;
 
+    @Autowired
+    private RedisService redisService;
+
     @Override
     @Transactional
     public OrderDo createOrder(Integer uesrId, Integer itemId, Integer promoId, Integer amount) throws BussinesssError {
@@ -51,11 +54,11 @@ public class OrderServiceImpl implements OrderService {
             throw new BussinesssError(ErrorEnum.PARAMTER_VALIDATION_ERROR, "购买数量有误");
         }
 
-        UserDO userDO = userDOMapper.selectByPrimaryKey(uesrId);
+        UserDO userDO = userInCache(uesrId);
         if (userDO == null)
             throw new BussinesssError(ErrorEnum.USER_NOT_EXIST);
 
-        ItemDto itemDto = itemService.getItemDetailById(itemId);
+        ItemDto itemDto = itemInCache(itemId);
         if (itemDto == null)
             throw new BussinesssError(ErrorEnum.ITEM_NOT_EXIST);
 
@@ -70,8 +73,6 @@ public class OrderServiceImpl implements OrderService {
                 throw new BussinesssError(ErrorEnum.PARAMTER_VALIDATION_ERROR, "活动信息不正确");
             else if (itemDto.getPromoDto().getStatus() != 2)
                 throw new BussinesssError(ErrorEnum.PARAMTER_VALIDATION_ERROR, "活动信息不正确");
-
-
         }
 
         // insert into order
@@ -117,13 +118,45 @@ public class OrderServiceImpl implements OrderService {
         // 凑足六位
         String sequenceStr = String.valueOf(sequence);
         for (int i = 0; i < 6 - sequenceStr.length(); i++) {
-            stringBuilder.append(0);
+            stringBuilder.append(i);
         }
         stringBuilder.append(sequenceStr);
 
         // 最后两位分库分表位
         stringBuilder.append("00");
         return stringBuilder.toString();
+    }
+
+
+    /**
+     * 缓存用户信息
+     */
+    private UserDO userInCache(Integer uesrId) {
+
+        String cacheKey = "user_valited_" + uesrId;
+        UserDO userDO = redisService.get(cacheKey, UserDO.class);
+
+        if(ObjectUtils.isEmpty(userDO)) {
+            userDO = userDOMapper.selectByPrimaryKey(uesrId);
+            redisService.put(cacheKey, new Gson().toJson(userDO), 10, TimeUnit.MINUTES);
+        }
+
+        return userDO;
+    }
+    /**
+     * 缓存商品信息
+     */
+    private ItemDto itemInCache(Integer id) {
+
+        String cacheKey = "item_valited_" + id;
+        ItemDto userDO = redisService.get(cacheKey, ItemDto.class);
+
+        if(ObjectUtils.isEmpty(userDO)) {
+            userDO = itemService.getItemDetailById(id);
+            redisService.put(cacheKey, new Gson().toJson(userDO), 10, TimeUnit.MINUTES);
+        }
+
+        return userDO;
     }
 
 }
